5e724347c3456747e85e5d26276c84dc6641a4cb
unknown <laxmiskarbari@gmail>
2024-02-23 17:46:02 +0530

    Created  a NavBar and Added 3 list components.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
+import "./App.css"
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
 import ArticlelistPage from "./pages/ArticleslistPage";
 import HomePage from "./pages/HomePage";
 import NotfoundPage from "./pages/NotfoundPage";
+import NavBar from "./NavBar"
 
 function App(){
   return(
@@ -11,7 +13,9 @@ function App(){
     <BrowserRouter>
     <div className="App">
       <h1> first react App</h1>
+      <NavBar></NavBar>
       <div id="Page-body">
+        
       <Routes>
       <Route path="/" element={<HomePage/>}></Route>
       <Route path="/About" element={<AboutPage/>}></Route>
