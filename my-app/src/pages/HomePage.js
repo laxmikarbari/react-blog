0f3b914b8ddb551f7d30ec7cfcb530c1fa84daf3
unknown <laxmiskarbari@gmail>
2024-02-26 14:57:48 +0530

    dummy database article content .js is created

--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -1,6 +1,10 @@
 function HomePage(){
     return(
         <>
+        <h1> Dad joke </h1>
+        <p>Even though dad jokes might make us groan, we secretly love these fatherly zingers that are so bad they’re good, and maybe even brilliant. (Deny it if you must, person who just Googled “funny dad jokes.”)
+
+So, in honor of joke-telling dads everywhere, we present the best of the best corny dad jokes and puns, whether you need a few new one-liners to add to your own repertoire, are craving a good chuckle, or are looking for a good Father’s Day caption or dad quote to honor your hilarious pops. Get ready for the eye rolls, because we're coming in hot.</p>
          HomePage
         </>
     );
