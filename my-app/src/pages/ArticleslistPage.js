7b3ef7eb5f20b055f6cf13707a7bd6fc75e350ec
unknown <laxmiskarbari@gmail>
2024-02-28 16:09:14 +0530

    Created back-end folder. Created server api.Created dummy database articleInfo.

--- my-app/src/pages/ArticleslistPage.js
+++ my-app/src/pages/ArticleslistPage.js
@@ -1,17 +1,12 @@
 import article from "./article-content";
-import {Link} from "react-router-dom";
+import ArticleList from "../components/ArticleList";
 
 
 function ArticlelistPage(){
     return(
         <>
        <h1>list of articles</h1>
-       {article.map ((article) =>(
-        <Link key = {article.name} className="article-list-item" to={`/articles/${article.name}` }> 
-        <h3>{article.title}</h3>
-        <p>{article.content[0].substring(0,100)}....</p>      
-             </Link>
-       ))}
+       <ArticleList articles = {article}></ArticleList>
         </>
     );
 }
